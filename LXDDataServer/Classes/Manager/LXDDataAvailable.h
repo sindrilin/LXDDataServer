a2b4ab3d20033fc49a2acc0f98dd30be8780531a
sindrilin <634893985@qq.com>
2017-08-27 10:06:51 +0800

    将部分协议方法改为可选实现

--- LXDDataServer/Classes/Manager/LXDDataAvailable.h
+++ LXDDataServer/Classes/Manager/LXDDataAvailable.h
@@ -19,7 +19,6 @@ typedef void(^DataFetchHandle)(NSArray *datas,  NSError * _Nullable error);
 @protocol LXDDataAvailable <NSObject>
 
 @required
-#pragma mark Fetch
 /*!
  *  @Method fetchAllDataWithFetchHandle:
  *  拉取全部数据，不适合翻页数据
@@ -27,30 +26,11 @@ typedef void(^DataFetchHandle)(NSArray *datas,  NSError * _Nullable error);
 - (void)fetchAllDataWithFetchHandle: (DataFetchHandle)fetchHandle;
 
 /*!
- *  @Method fetchDatasInRange:fetchHandle:
- *  获取range范围的数据
- */
-- (void)fetchDatasInRange: (NSRange)range fetchHandle: (DataFetchHandle)fetchHandle;
-
-/*!
  *  @Method fetchDatasAtIndex:fetchHandle:
  *  获取下标对应的数据
  */
 - (void)fetchDatasAtIndex: (NSUInteger)index fetchHandle: (DataFetchHandle)fetchHandle;
 
-/*!
- *  @Method fetchDatasFromServerWithPage:complete:
- *  适合翻页界面调用，请求对应页数的数据
- */
-- (void)fetchDatasFromServerWithPage: (NSUInteger)page complete: (DataFetchHandle)complete;
-
-/*!
- *  @Method fetchDatasAtIndexSet:fetchHandle:
- *  请求indexSet当中所有对应下标的数据
- */
-- (void)fetchDatasAtIndexSet: (NSIndexSet *)indexSet fetchHandle: (DataFetchHandle)fetchHandle;
-
-#pragma mark Insert
 /*!
  *  @Method appendData:
  *  追加数据
@@ -63,25 +43,11 @@ typedef void(^DataFetchHandle)(NSArray *datas,  NSError * _Nullable error);
  */
 - (void)insertData: (id)data atIndex: (NSUInteger)index;
 
-#pragma mark Replace
 /*!
  *  @Method replaceData:atIndex:
  *  替换下标位置的数据
  */
 - (void)replaceData: (id)data atIndex: (NSUInteger)index;
-
-#pragma mark Delete
-/*!
- *  @Method dropLastData
- *  删除最后一个数据
- */
-- (void)dropLastData;
-
-/*!
- *  @Method dropFirstData
- *  删除第一个数据
- */
-- (void)dropFirstData;
 
 /*!
  *  @Method deleteDataAtIndex:
@@ -95,7 +61,43 @@ typedef void(^DataFetchHandle)(NSArray *datas,  NSError * _Nullable error);
  */
 - (void)deleteDataInIndexSet: (NSIndexSet *)indexSet;
 
-#pragma mark Sort
+/*!
+ *  @Method syncDatasToServer
+ *  向服务器发起同步请求
+ */
+- (void)syncDatasToServer;
+
+@optional
+/*!
+ *  @Method fetchDatasInRange:fetchHandle:
+ *  获取range范围的数据
+ */
+- (void)fetchDatasInRange: (NSRange)range fetchHandle: (DataFetchHandle)fetchHandle;
+
+/*!
+ *  @Method fetchDatasFromServerWithPage:complete:
+ *  适合翻页界面调用，请求对应页数的数据
+ */
+- (void)fetchDatasFromServerWithPage: (NSUInteger)page complete: (DataFetchHandle)complete;
+
+/*!
+ *  @Method fetchDatasAtIndexSet:fetchHandle:
+ *  请求indexSet当中所有对应下标的数据
+ */
+- (void)fetchDatasAtIndexSet: (NSIndexSet *)indexSet fetchHandle: (DataFetchHandle)fetchHandle;
+
+/*!
+ *  @Method dropLastData
+ *  删除最后一个数据
+ */
+- (void)dropLastData;
+
+/*!
+ *  @Method dropFirstData
+ *  删除第一个数据
+ */
+- (void)dropFirstData;
+
 /*!
  *  @Method sortByUsingSelector:
  *  使用指定的方法对数据进行排序，方法应该返回NSComparisonResult结果
@@ -107,13 +109,6 @@ typedef void(^DataFetchHandle)(NSArray *datas,  NSError * _Nullable error);
  *  使用block对数组数据进行排序
  */
 - (void)sortByUsingCompare: (NSComparisonResult(^)(id data1, id data2))compare;
-
-#pragma mark Sync
-/*!
- *  @Method syncDatasToServer
- *  向服务器发起同步请求
- */
-- (void)syncDatasToServer;
 
 @end
 
