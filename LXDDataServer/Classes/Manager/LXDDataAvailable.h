cad1976953e28d60c64b08929e2893bf66b16b56
sindrilin <634893985@qq.com>
2017-08-27 09:54:50 +0800

    修正提交bug

--- LXDDataServer/Classes/Manager/LXDDataAvailable.h
+++ LXDDataServer/Classes/Manager/LXDDataAvailable.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-typedef void(^DataFetchHandle)(NSArray *datas, _Nullable NSError *error);
+typedef void(^DataFetchHandle)(NSArray *datas,  NSError * _Nullable error);
 
 /*!
  *  @protocol   LXDDataAvailable
